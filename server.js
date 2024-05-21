21122ddf1752192acc0598f963851a31df2b63f6
SleepingAmi <143962821+SleepingAmi@users.noreply.github.com>
2024-05-21 19:21:40 +0200

    Let's make some bloggers ig

---
+++ server.js
@@ -0,0 +1,143 @@
+const express = require('express');
+const ejs = require('ejs');
+const firebase = require('firebase');
+const bodyParser = require('body-parser');
+
+const { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId } = require('./firebase_env.json')
+
+// start express + extras
+const port = process.env.port || 8800;
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+const firebaseConfig = {
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
+    measurementId: measurementId
+};
+const dbapp = firebase.initializeApp(firebaseConfig);
+const store = firebase.firestore();
+
+// listen to app
+app.listen(port, function(){
+    console.log('Ready to Hy some Draulisc! Port: ' + port);
+})
+
+
+function cleanseHTML(text) {
+    const cleansedHTML = text.replace(/[<>"&]/g, function (match) {
+      return {
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        '&': '&amp;',
+      }[match];
+    });
+    return cleansedHTML;
+}
+
+
+// login page
+app.get('/auth', function (req, res) {
+    const checkUser = firebase.auth().currentUser;
+    var userID = "";
+    if(checkUser) {
+      userID = checkUser.uid;
+    } else {
+      userID = undefined;
+    }
+    if(userID) {
+      res.redirect(`/`)
+    } else {
+      res.render('pages/auth', {
+        title: null,
+        source: "",
+        username: userID,
+        uid: req.uid,
+      });
+    }
+  });
+
+
+  /*  WARNING!
+
+    authorise existing user
+
+    **WARNING!!** DO NOT USE THIS AUTH METHOD IN PRODUCTION. IT HAS BEEN PROVEN (COUNTLESS TIMES) THAT IT STORES THE USER INFO LOCALLY ON THE SERVER,
+    CREATING A SEVERE SECURITY VULNERABILITY.
+
+  */
+
+app.post('/auth/login', async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      const userId = firebase.auth().currentUser.uid;
+      res.status(200).send('OK');
+    } catch (error) {
+      const errorMessage = error.message;
+      console.error(errorMessage);
+      res.status(500).send(errorMessage);
+    }
+  });
+
+app.get('/', async (req, res) => {
+    var checkUser = "";
+    if(firebase.auth().currentUser != null) checkUser = firebase.auth().currentUser;
+    var userID = "";
+    if(checkUser) {
+      userID = checkUser.uid;
+    } else {
+      userID = undefined;
+    }
+    res.render('pages/blogger', {
+      username: "SleepingAmi",
+      uid: firebase.auth().currentUser
+    });
+})
+
+/* UPLOAD BLOG ENTRIES
+
+    This is used to upload the actual blog posts. Don't ask what kind of wizardry goes into this. We just know it works.
+
+*/
+
+app.post('/post/create', async (req, res) => {
+  const preSanitizedTitle = req.body.title;
+  const preSanitizedBlog = req.body.content;
+
+  // Check if any of the required fields are blank
+  if (!preSanitizedTitle || preSanitizedTitle.trim().length === 0 || !preSanitizedBlog || preSanitizedBlog.trim().length === 0) {
+    return res.status(400).send('Blog Title and Post Body are all required fields.');
+  }
+  
+  try {
+    // Get the count of existing documents in the "entries" collection
+    const snapshot = await store.collection('entries').get();
+    const docCount = snapshot.size;
+
+    const blogPost = {
+      title: cleanseHTML(preSanitizedTitle),
+      content: cleanseHTML(preSanitizedBlog),
+      pnum: docCount + 1
+    }
+
+    const docRef = await store.collection('entries').add(blogPost);
+    const postId = docRef.id;
+    const timestamp = firebase.firestore.FieldValue.serverTimestamp();
+    const updatedPost = { path: postId, update: timestamp };
+
+    await docRef.update(updatedPost);
+    res.status(200).json({ uploadStatus: 'success' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error patching blog post: ' + error.message });
+  }
+})
